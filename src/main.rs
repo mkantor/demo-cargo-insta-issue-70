2ec3db4d1232cd74af2e70378620339655505dc5
Matt Kantor <the.matt.kantor@gmail.com>
2020-09-01 11:18:29 -0700

    Reproduce issue #70.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+#[test]
+fn repro() {
+    use insta::{assert_snapshot, Settings};
+    use std::path::Path;
+    use std::time::SystemTime;
+
+    let non_constant_value = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        .to_string();
+
+    let mut settings = Settings::new();
+    let snapshot_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("snapshots");
+    settings.set_snapshot_path(snapshot_path);
+    settings.bind(|| assert_snapshot!(non_constant_value));
 }
